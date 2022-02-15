cb3a36bcd51eb272c20b607ebcb411aba584fd28
gauravtirodkar <gauravtirodkar42@gmail.com>
2022-02-15 14:54:00 +0530

    build commit

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,9 @@
 #! /usr/bin/python3
 
-print("Hello World!!!...\nThe pipeline is working")
+print("Hello World !!!...\nThe pipeline is working")
+print("Hello World !!!...\nThe pipeline is working")
+
+print("Hello World !!!...\nThe pipeline is working")
+
+print("Hello World !!!...\nThe pipeline is working")
+
