4aa6be0037c7ebde38ba10544e31b102e171c411
gauravtirodkar <gauravtirodkar42@gmail.com>
2022-02-16 14:27:25 +0530

    Testing

---
+++ Test.py
@@ -0,0 +1,11 @@
+import unittest
+from Prog1 import summation
+
+class TestSum(unittest.TestCase):
+    def test_list_init(self):
+        data = [2,3]
+        result = summation(data)
+        self.assertEqual(result,5)
+
+if __name__ == '__main__':
+    unittest.main()
